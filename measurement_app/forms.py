22c9333381a05cbdb3b44b21bc2ea8e94353c1ab
Ahsan Habib <shehanuk.ahsan@gmail.com>
2021-08-18 22:40:41 +0600

    backend is ready to connect with frontend, form+output is ready

--- measurement_app/forms.py
+++ measurement_app/forms.py
@@ -11,3 +11,9 @@ class officeDetailForm(forms.ModelForm):
     class Meta:
         model = officeDetail
         fields = '__all__'
+        '''
+        widgets = {
+            'office_size': forms.NumberInput(attrs={'label': 'Your office Size? :','required': True}),
+            'office_type': forms.FileInput(attrs={'label': 'Your office type? :', 'required': True}),
+        }
+        '''
--- measurement_app/views.py
+++ measurement_app/views.py
@@ -1,4 +1,6 @@
 import math
+from django.contrib import messages
+from measurement_app.forms import officeDetailForm
 from django.shortcuts import render, redirect
 from django.http import Http404
 from math import ceil
@@ -10,22 +12,29 @@ from .forms import
 '''
 app_name = 'Fire Guardian'
 
-def office_all_equpments(office, fe_param):
+def prototype_output(office):
     sz = office.office_size
 
     if office.office_type == 'in':
-        required_extenguiter = office.present_fire_extinguisher_number - math.ceil(sz/1000) 
+        type = 'Industrial'
+        required_extenguiter = math.ceil(sz/1000) - office.present_fire_extinguisher_number 
     elif office.office_type == 'cm':
-        required_extenguiter = math.ceil(sz/3000) 
+        type = 'Commercial'
+        required_extenguiter = math.ceil(sz/3000) - office.present_fire_extinguisher_number 
     elif office.office_type == 'rs':
-        required_extenguiter = math.ceil(sz/5000) 
+        type = 'Residential'
+        required_extenguiter = math.ceil(sz/5000) - office.present_fire_extinguisher_number
  
-    #office_dict = {'type': office
-
+    op = f'Hlw! wellcome! Your are currently in a {type} building. your space is {sz} sqft. You need { required_extenguiter } Fire Extenguiser'
+    if required_extenguiter <= 0:
+        op = f'Hlw! wellcome! Your are currently in a {type} building. your space is {sz} sqft. Acording to Bangladesh low, you have enough equpments'
+    return op
 
 def home(request):
+    form = officeDetailForm()
     contex = {
         'key': 'value',
+        'form': form,
     }
     return render(request, 'home.html', contex)
 
@@ -62,19 +71,32 @@ def all_office(request):
 
 def last_entry_view(request):
     header = 'Last Entry'
+    if request.method == 'POST':
+        form = officeDetailForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'successfully!!')
+        else:
+            messages.success(request, 'eror !!')
+
+        redirect(last_entry_view)
+   
     try:
         all_office = officeDetail.objects.all()
     except officeDetail.DoesNotExist:
         raise Http404("No Model, Pls check migration.") 
+    
+    last_office = all_office[len(all_office)-1]
 
-    last_office = all_office.reverse()[0]
+    op = prototype_output(last_office)
      
     contex = {
         'last_office': last_office,
         'header' : header,
         'key': 'value',
         'app_name' : app_name,
-        'sz': sz,
-        'required_extenguiter': required_extenguiter,
+        'op': op,
+        #'sz': sz,
+        #'required_extenguiter': required_extenguiter,
     }
     return render(request, 'last_entry.html', contex)
